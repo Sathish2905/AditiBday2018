53ac9d59a7d6854b50d2ad83e2d9a3db3c0d275d
Sathish2905 <sathishkumart@logiwareinc.in>
2018-05-28 22:47:51 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# AditiBday2018
---
+++ js/functions.js
@@ -0,0 +1,26 @@
+﻿(function($) {
+	$.fn.typewriter = function() {
+		this.each(function() {
+			var $ele = $(this), str = $ele.html(), progress = 0;
+			$ele.html('');
+			var timer = setInterval(function() {
+				var current = str.substr(progress, 1);
+				if (current == '<') {
+					progress = str.indexOf('>', progress) + 1;
+				} else {
+					progress++;
+				}
+				$ele.html(str.substring(0, progress) + (progress & 1 ? '' : ''));
+				if (progress >= str.length) {
+					clearInterval(timer);
+				}
+			}, 75);
+		});
+		return this;
+	};
+})(jQuery);
+
+function adjustCodePosition() {
+	$('#code').css("margin-top", -15);
+}
+
